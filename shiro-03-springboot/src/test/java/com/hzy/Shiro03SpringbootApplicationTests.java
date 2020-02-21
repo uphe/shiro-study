8ad5a41def6d5507e530f95ffccdca09e75378db
uphe <1185218427@qq.com>
2020-02-21 17:32:09 +0800

    first commit

---
+++ shiro-03-springboot/src/test/java/com/hzy/Shiro03SpringbootApplicationTests.java
@@ -0,0 +1,20 @@
+package com.hzy;
+
+import com.hzy.pojo.User;
+import com.hzy.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shiro03SpringbootApplicationTests {
+
+    @Autowired
+    private UserService userService;
+    @Test
+    void contextLoads() {
+        User user = userService.queryUserByName("root");
+        System.out.println(user);
+    }
+
+}
